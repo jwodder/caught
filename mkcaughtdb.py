a5d3d07baf301421f27f6cca1916a325e1781ef6
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-01-10 22:49:35 -0500

    Don't mix tabs & spaces

--- mkcaughtdb.py
+++ mkcaughtdb.py
@@ -15,44 +15,44 @@ PRAGMA foreign_keys = ON;
 PRAGMA encoding = "UTF-8";
 
 CREATE TABLE pokemon (dexno INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		      name  TEXT    NOT NULL UNIQUE);
+                      name  TEXT    NOT NULL UNIQUE);
 
 CREATE TABLE pokemon_names (dexno INTEGER NOT NULL REFERENCES pokemon(dexno),
-			    name  TEXT    NOT NULL UNIQUE,
-			    CHECK (name = lower(name)));
+                            name  TEXT    NOT NULL UNIQUE,
+                            CHECK (name = lower(name)));
 
 CREATE TABLE games (gameID      INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		    version     TEXT    NOT NULL,
-		    player_name TEXT    NOT NULL,
-		    dexsize     INTEGER NOT NULL);
+                    version     TEXT    NOT NULL,
+                    player_name TEXT    NOT NULL,
+                    dexsize     INTEGER NOT NULL);
 
 CREATE TABLE game_names (gameID INTEGER NOT NULL REFERENCES games(gameID),
-			 name   TEXT    NOT NULL UNIQUE,
-			 CHECK (name = lower(name)));
+                         name   TEXT    NOT NULL UNIQUE,
+                         CHECK (name = lower(name)));
 
 CREATE TABLE caught (gameID INTEGER NOT NULL REFERENCES games(gameID),
-		     dexno  INTEGER NOT NULL REFERENCES pokemon(dexno),
-		     status INTEGER NOT NULL,
-		     PRIMARY KEY (gameID, dexno));
+                     dexno  INTEGER NOT NULL REFERENCES pokemon(dexno),
+                     status INTEGER NOT NULL,
+                     PRIMARY KEY (gameID, dexno));
 ''')
 
     with open(pokedex) as dex:
-	for (lineno, line) in enumerate(dex, start=1):
-	    line = line.strip()
-	    if line == '' or line[0] == '#':
-		continue
-	    fields = line.split('\t')
-	    if len(fields) < 2:
-		raise SystemExit('%s: %s: line %d: too few fields'
-				 % (sys.argv[0], pokedex, lineno))
-	    try:
-		dexno = int(fields[0])
-	    except ValueError:
-		raise SystemExit('%s: %s: line %d: %s: not a number'
-				 % (sys.argv[0], pokedex, lineno, fields[0]))
-	    db.execute('INSERT OR ROLLBACK INTO pokemon (dexno, name)'
-		       ' VALUES (?,?)', (dexno, fields[1]))
-	    db.executemany('INSERT OR ROLLBACK INTO pokemon_names (dexno, name)'
-			   ' VALUES (?,?)', ((dexno, name.lower())
-					     for name in fields))
+        for (lineno, line) in enumerate(dex, start=1):
+            line = line.strip()
+            if line == '' or line[0] == '#':
+                continue
+            fields = line.split('\t')
+            if len(fields) < 2:
+                raise SystemExit('%s: %s: line %d: too few fields'
+                                 % (sys.argv[0], pokedex, lineno))
+            try:
+                dexno = int(fields[0])
+            except ValueError:
+                raise SystemExit('%s: %s: line %d: %s: not a number'
+                                 % (sys.argv[0], pokedex, lineno, fields[0]))
+            db.execute('INSERT OR ROLLBACK INTO pokemon (dexno, name)'
+                       ' VALUES (?,?)', (dexno, fields[1]))
+            db.executemany('INSERT OR ROLLBACK INTO pokemon_names (dexno, name)'
+                           ' VALUES (?,?)', ((dexno, name.lower())
+                                             for name in fields))
     db.commit()
